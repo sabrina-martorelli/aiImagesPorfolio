bcbf2ab1ac47f277da3988724043c01b5c5ca445
SLM <sabrina.martorelli@gmail.com>
2023-06-05 17:09:55 +0100

    Adds MainImage component

---
+++ src/components/MainImage.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { makeStyles } from '@mui/styles';
+import MainText from './MainText';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    display: 'flex',
+    justifyContent: 'center',
+    alignItems: 'center',
+    height: '100vh',
+    backgroundImage: 'url(https://miro.medium.com/v2/resize:fit:720/format:webp/1*jbvqG9vT9ishTLzl_CVbiw.jpeg)',
+    backgroundSize: 'cover',
+    backgroundPosition: 'center',
+  },
+}));
+
+const MainImage = () => {
+  const classes = useStyles();
+
+  return <div className={classes.root}> <MainText/></div>;
+};
+
+export default MainImage;
