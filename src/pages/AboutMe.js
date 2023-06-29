6ea2042828987a7e1c15fe9a9a18dfa54bfb7c48
SLM <sabrina.martorelli@gmail.com>
2023-06-29 18:35:44 +0100

    Adds json files for data

--- src/pages/AboutMe.js
+++ src/pages/AboutMe.js
@@ -2,14 +2,7 @@
 import React, { useState } from 'react';
 import { makeStyles } from '@mui/styles';
 import AboutMeText from '../components/AboutMeText';
-
-
-
-
-//https://cdn.pixabay.com/photo/2023/02/13/16/14/ai-generated-7787714_1280.jpg
-
-
-
+import urls from '../data/dataAboutMe.json'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -23,14 +16,9 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const AboutMe = () => {
-    const urls = [
-     'https://cdn.pixabay.com/photo/2023/02/13/16/14/ai-generated-7787714_1280.jpg',
-     'https://www.verke.org/uploads/2022/08/d3c974c4-panu_giant_robot_standing_in_the_center_of_a_beautiful_landscap_b555fb6f-fb9c-4bb1-827d-ef7b1df41a7a-1600x800.png',
-     'https://images.nightcafe.studio/jobs/cNxmQICjMo8xdPi31Zjt/cNxmQICjMo8xdPi31Zjt_6.9444x.jpg?tr=w-1600,c-at_max',
-     'https://miro.medium.com/v2/resize:fit:1400/0*QvtiBBzbdBOCRF-o.jpeg',
-     'https://static01.nyt.com/images/2022/09/01/business/00roose-1/merlin_212276709_3104aef5-3dc4-4288-bb44-9e5624db0b37-superJumbo.jpg',
-     'https://etinsights.et-edge.com/wp-content/uploads/2022/09/shutterstock_2197962897.jpg',
-      ];
+   
+  
+
     
  
       const classes = useStyles();
@@ -38,7 +26,8 @@ const AboutMe = () => {
     
       const getRandomImage = () => {
         const randomIndex = Math.floor(Math.random() * urls.length);
-        setRandomImage(urls[randomIndex]);
+      
+        setRandomImage(urls[randomIndex].url);
       };
     
       
--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -3,6 +3,7 @@
 import LandingText from '../components/LandingText'
 import React, { useState } from 'react';
 import { makeStyles } from '@mui/styles';
+import urls from '../data/dataLanding.json'
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -16,22 +17,14 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Landing = () => {
-    const urls = [
-        'https://cdn.pixabay.com/photo/2023/03/06/11/39/ai-generated-7833307_960_720.jpg',
-        'https://cdn.pixabay.com/photo/2022/11/23/22/27/man-7613011_1280.jpg',
-        'https://miro.medium.com/v2/resize:fit:720/format:webp/1*jbvqG9vT9ishTLzl_CVbiw.jpeg',
-        'https://img.i-scmp.com/cdn-cgi/image/fit=contain,width=1098,format=auto/sites/default/files/styles/1200x800/public/d8/images/canvas/2022/12/21/e9cb4da8-8867-447b-910e-3fd9cafb9516_8c570677.jpg?itok=WizBCCZy&v=1671560574',
-        'https://storage.googleapis.com/cdn.openart.ai/presets/cover_image20_4_edited.jpeg',
-        'https://www.analyticsinsight.net/wp-content/uploads/2023/04/8-FREE-NSFW-AI-Art-Generators-to-Create-Images-from-Text.jpg',
-      ];
-    
+
  
       const classes = useStyles();
       const [randomImage, setRandomImage] = useState('');
     
       const getRandomImage = () => {
         const randomIndex = Math.floor(Math.random() * urls.length);
-        setRandomImage(urls[randomIndex]);
+        setRandomImage(urls[randomIndex].url);
       };
     
       
